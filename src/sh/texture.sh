9088f3930f56e533e8675b005e80a510f786bb2b
tomasparks <tomasparks@users.noreply.github.com>
2017-04-02 19:26:27 +1000

    temp texturing 2

--- src/sh/texture.sh
+++ src/sh/texture.sh
@@ -1,8 +1,14 @@
 convert -verbose -seed 200 \
           -size 1024x1024 plasma:fractal -blur 0x15 -colorspace Gray \
-  /opt/lampp/htdocs/retrospacegame/data/textures/generic/planets/8Bit/assets/minecraft/textures/blocks/{water_still,sand,grass_top,snow}.png \
+  /opt/lampp/htdocs/retrospacegame/data/textures/generic/planets/8Bitv1/assets/minecraft/textures/blocks/{water_still,sand,grass_top,snow}.png \
   -virtual-pixel tile  -fx 'u[floor(4.9999*u)+1]' \
-  map-8Bit.png
+  map-8Bitv1.png
+
+convert -verbose -seed 200 \
+          -size 1024x1024 plasma:fractal -blur 0x15 -colorspace Gray \
+  /opt/lampp/htdocs/retrospacegame/data/textures/generic/planets/8Bitv2/assets/minecraft/textures/blocks/{water_still,sand,grass_top,snow}.png \
+  -virtual-pixel tile  -fx 'u[floor(4.9999*u)+1]' \
+  map-8Bitv2.png
 
 convert -verbose -seed 200 \
           -size 1024x1024 plasma:fractal -blur 0x15 -colorspace Gray \
